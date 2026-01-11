4136a3144d485bfc3b01f7d1e65f32b47e944d52
John Swift <johnswift@Johns-Air.hsd1.va.comcast.net>
2026-01-11 17:31:30 -0500

    docs: update notification screenshot

--- cmd/claudible/main.go
+++ cmd/claudible/main.go
@@ -23,7 +23,7 @@ import (
 
 const (
 	// Version is the current version of Claudible.
-	Version = "0.5.3"
+	Version = "0.5.4"
 
 	// DefaultTimeout is the default context timeout for processing.
 	DefaultTimeout = 30 * time.Second
